dc8ff13ff0724e4d44b6274406a1adb052c344b5
rcpd <5835162+rcpd@users.noreply.github.com>
2024-03-12 20:52:09 +1100

    Dev improvements (#5)
    * Add Sphinx docs + doc strings
    
    * Strip ANSI colour codes from file based logs
    
    * Argparse formatting
    
    * Add Sphinx steps to docs
    
    * Convert to module + package
    
    * Ruff formatting
    
    * Tox
    
    * Update notes for -m run
    
    * Initial tests (wip)
    
    * Add sphinx (darglint) + typing (ruff) linting
    
    * Doc strings + function breakout
    
    * Separate _main() run
    
    * Update arg processing
    
    * Linting, docs, functions
    
    * Fetch
    
    * TODOs
    
    * Fix docs
    
    * Add pydoclint + pyproject.toml
    
    * Docs
    
    * Fix branch parsing + list branch test
    
    * Test find_root_branch
    
    * Add upstream parsing + test
    
    * Branch functions + ahead test
    
    * Fix "ahead" test
    
    * wip
    
    * Update CRUD test
    
    * Cleanup remotes
    
    * TODO
    
    * Docs + formatting

---
+++ index.rst
@@ -0,0 +1,7 @@
+Git-Garden
+==========
+
+.. autoclass:: git_garden.GitGarden
+   :members:
+   :undoc-members:
+   :private-members:
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="git-garden",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "ruff",
+            "sphinx",
+            "sphinx-autodoc-typehints",
+            "tox",
+            "pytest",
+            "pytest-cov",
+            "pydoclint",
+        ]
+    },
+)
