cf4ee976b9d9bf63291d4df8a83dd343303dbccb
rcpd <5835162+rcpd@users.noreply.github.com>
2024-04-09 17:37:30 +1000

    Branch imports for typing (3.10+)

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import sys
 from setuptools import setup, find_packages
+
+if sys.version_info < (3, 10):
+    install_requires = ["typing-extensions"]
+else:
+    install_requires = []
 
 setup(
     name="git-garden",
-    version="0.2",
+    version="0.2.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=install_requires,
     extras_require={
         "dev": [
             "ruff",
