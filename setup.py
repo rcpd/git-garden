d074a5ae9c35cd94d7b192043bae83c0e99df793
rcpd <5835162+rcpd@users.noreply.github.com>
2024-05-14 20:42:23 +1000

    Hotfix: Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ if sys.version_info < (3, 10):
 
 setup(
     name="git-garden",
-    version="0.3.1",
+    version="0.3.3",
     packages=find_packages(),
     install_requires=install_requires,
     extras_require={"dev": dev_dependencies},
