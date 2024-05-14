9ea748bd5fd66ba9af663ec31928acbb2186751b
rcpd <5835162+rcpd@users.noreply.github.com>
2024-05-14 17:00:33 +1000

    First pass

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 import sys
 from setuptools import setup, find_packages
 
+dev_base = [
+    "ruff",
+    "sphinx",
+    "sphinx-autodoc-typehints",
+    "tox",
+    "pytest",
+    "pytest-cov",
+    "mypy",
+]
+
+if sys.version_info >= (3, 8):
+    dev_dependencies = dev_base + ["pydoclint"]
+else:
+    dev_dependencies = dev_base
+
 if sys.version_info < (3, 10):
     install_requires = ["typing-extensions"]
 else:
@@ -11,16 +26,5 @@ setup(
     version="0.3.1",
     packages=find_packages(),
     install_requires=install_requires,
-    extras_require={
-        "dev": [
-            "ruff",
-            "sphinx",
-            "sphinx-autodoc-typehints",
-            "tox",
-            "pytest",
-            "pytest-cov",
-            "pydoclint",
-            "mypy",
-        ]
-    },
+    extras_require={"dev": dev_dependencies},
 )
