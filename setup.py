b15195ba7157c314f4b0d11797e00bcb905d8f36
rcpd <5835162+rcpd@users.noreply.github.com>
2024-05-14 17:34:39 +1000

    py37-38 parsing (#11)
    * First pass
    
    * Fix version isolation
    
    * Refactor

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 import sys
 from setuptools import setup, find_packages
 
+install_requires = []
+dev_dependencies = [
+    "ruff",
+    "sphinx",
+    "sphinx-autodoc-typehints",
+    "tox",
+    "pytest",
+    "pytest-cov",
+    "mypy",
+]
+
+if sys.version_info >= (3, 8):
+    dev_dependencies.append("pydoclint")
+
 if sys.version_info < (3, 10):
-    install_requires = ["typing-extensions"]
-else:
-    install_requires = []
+    install_requires.append("typing-extensions")
 
 setup(
     name="git-garden",
     version="0.3.1",
     packages=find_packages(),
     install_requires=install_requires,
-    extras_require={
-        "dev": [
-            "ruff",
-            "sphinx",
-            "sphinx-autodoc-typehints",
-            "tox",
-            "pytest",
-            "pytest-cov",
-            "pydoclint",
-            "mypy",
-        ]
-    },
+    extras_require={"dev": dev_dependencies},
 )
